2f706cda70f05ce64e5fbb0c6a6cf63e4b103344
clixter <77027229+clixter@users.noreply.github.com>
2022-03-07 22:45:59 +0100

    Proyecto GutierrezMurilloFcoJavier2122 comentado con JavaDoc

--- src/huevoSorpresa/HuevoSorpresa.java
+++ src/huevoSorpresa/HuevoSorpresa.java
@@ -1,6 +1,12 @@
 // No se está usando un nombre convencional en el paquete.
 package huevoSorpresa;
 
+/**
+ * Clase que representa un conjunto de huevos sorpresa. Contendrá información
+ * relativa al número de huevos, precio, sorpresa y precio máximo.
+ *
+ * @author Francisco Javier Gutiérrez Murillo.
+ */
 public class HuevoSorpresa {
 
     private int unidades;//número de huevos que quedan 
@@ -9,12 +15,25 @@ public class HuevoSorpresa {
     // No se está usando un nombre convencional en la variable.
     private double precio_maxGutierrezMurilloFcoJavier2122;//precio máximo que puede tener un huevo sorpresa
 
+    // Constructores
+    // -------------
     /*Constructor por defecto*/
+    /**
+     * Constructor de Huevo Sorpresa sin parámetros.
+     *
+     */
     public HuevoSorpresa() {
     }
 
-
-    /*Constructor por parámetros*/
+    // Constructor con cuatro parámetros
+    /**
+     * Constructor de Huevo Sorpresa con cuatro parámetros.
+     *
+     * @param unidades Número de unidades.
+     * @param precio Precio del Huevo Sorpresa.
+     * @param sorpresa Sorpresa del Huevo.
+     * @param precio_max Precio máximo.
+     */
     // No se está usando un nombre convencional en la variable.
     public HuevoSorpresa(int unidades, double precio, String sorpresa, double precio_max) {
         this.unidades = unidades;
@@ -23,37 +42,72 @@ public class HuevoSorpresa {
         this.precio_maxGutierrezMurilloFcoJavier2122 = precio_max;
     }
 
-
-    /*Método que devuelve el número de huevos sorpresa que tiene la tienda*/
+    // Método obtenerUnidades
+    /**
+     * Obtiene el número de huevos sorpresa que tiene la tienda.
+     *
+     * @return Número de huevos sorpresa que tiene la tienda.
+     */
     public int obtenerUnidades() {
         return this.getUnidades();
     }
 
-    /*Método que devuelve el precio que tiene cada huevo sorpresa*/
+    // Método obtenerPrecio
+    /**
+     * Obtiene el precio que tiene el huevo sorpresa.
+     *
+     * @return Precio que tiene cada huevo sorpresa.
+     */
     public double obtenerPrecio() {
         return this.getPrecio();
     }
 
-
-    /* Método que devuelve la sorpresa que tiene el huevo*/
+    // Método obtenerSorpresa
+    /**
+     * Obtiene la sorpresa que tiene el huevo.
+     *
+     * @return Sorpresa que tiene el huevo.
+     */
     public String obtenerSorpresa() {
         return getSorpresa();
     }
 
-    /* Método que permite modificar la sorpresa del huevo*/
+    // Método modificarSorpresa
+    /**
+     * Modifica la sorpresa que tiene el huevo.
+     *
+     * @param sorpresa Sorpresa del Huevo.
+     *
+     */
     public void modificarSorpresa(String sorpresa) {
         this.setSorpresa(sorpresa);
     }
 
-
-    /*Método que permite modificar el número de huevos sorpresa que tiene la tienda*/
+    // Método modificarUnidades
+    /**
+     * Modifica el número de huevos sorpresa que tiene la tienda.
+     *
+     * @param unidades Número de huevos.
+     *
+     */
     public void modificarUnidades(int unidades) {
         this.setUnidades(unidades);
     }
 
 
     /*Método que permite sacar huevos sorpresa si se tiene suficiente dinero y hay 
-suficientes unidades en la tienda*/
+    suficientes unidades en la tienda*/
+    /**
+     * Saca huevos sorpresa si se tiene suficiente dinero y hay suficientes
+     * unidades en la tienda
+     *
+     * @param unidades Huevos a aumentar.
+     * @param dinero Dinero a aumentar.
+     * @param sorpresaGutierrezMurilloFcoJavier2122 Precio a aumentar.
+     *
+     * @throws Exception si la cantidad de dinero a aumentar o de huevos es
+     * incorrecta.
+     */
     // No se debe de usar la Exception genérica y no se está usando un nombre convencional en la variable.
     public void sacarHuevosSorpresas(int unidades, double dinero, String sorpresaGutierrezMurilloFcoJavier2122) throws Exception {
         if (dinero <= 0) {
@@ -75,9 +129,18 @@ suficientes unidades en la tienda*/
         this.modificarUnidades(this.obtenerUnidades() - unidades);
     }
 
-
-    /*Método que permite aumentar el precio de venta de un huevo sorpresa, suma al precio actual 
-    el aumento que se indica siempre que no se sobrepase el precio máximo de venta*/
+    /*Método que permite aumentar el precio de venta de un huevo sorpresa. Suma al precio actual el
+     aumento que se indica siempre que no se sobrepase el precio máximo de
+     venta. descripcion Descripcion del dispositivo.*/
+    /**
+     * Aumenta el precio de venta de un huevo sorpresa. Suma al precio actual el
+     * aumento que se indica siempre que no se sobrepase el precio máximo de
+     * venta. descripcion Descripcion del dispositivo.
+     *
+     * @param aumento Precio a aumentar.
+     * @throws Exception si el Precio a aumentar es negativo o superior al
+     * máximo..
+     */
     // No se debe de usar la Exception genérica.
     public void aumentarPrecio(double aumento) throws Exception {
         if (aumento <= 0) {
@@ -91,60 +154,82 @@ suficientes unidades en la tienda*/
         setPrecio(getPrecio() + aumento);
     }
 
+    // Método getUnidades
     /**
-     * @return the unidades
+     * Obtiene el número de huevos que quedan.
+     *
+     * @return Número de huevos que quedan.
      */
     public int getUnidades() {
         return unidades;
     }
 
+    // Método setUnidades
     /**
-     * @param unidades the unidades to set
+     * Modifica el número de huevos que quedan.
+     *
+     * @param unidades Número de huevos que quedan.
      */
     public void setUnidades(int unidades) {
         this.unidades = unidades;
     }
 
+    // Método getPrecio
     /**
-     * @return the precio
+     * Obtiene el precio actual del huevo.
+     * @return Precio actual del Huevo.
      */
     public double getPrecio() {
         return precio;
     }
 
+    // Método setPrecio
     /**
-     * @param precio the precio to set
+     * Modifica el precio actual del huevo.
+     *
+     * @param precio Precio actual del huevo.
      */
     public void setPrecio(double precio) {
         this.precio = precio;
     }
 
+    // Método getSorpresa
     /**
-     * @return the sorpresa
+     * Obtiene la sorpresa del huevo.
+     * @return Sorpresa del Huevo.
      */
     public String getSorpresa() {
         return sorpresa;
     }
 
+    // Método setSorpresa
     /**
-     * @param sorpresa the sorpresa to set
+     * Modifica la sorpresa del huevo.
+     *
+     * @param sorpresa Sorpresa del huevo.
      */
     public void setSorpresa(String sorpresa) {
         this.sorpresa = sorpresa;
     }
 
+    // Método getPrecio_maxGutierrezMurilloFcoJavier2122
     /**
-     * @return the precio_maxGutierrezMurilloFcoJavier2122
+     * Obtiene el precio máximo del huevo.
+     * @return Precio máximo que puede tener un huevo sorpresa.
      */
     // No se está usando un nombre convencional en el método.
     public double getPrecio_maxGutierrezMurilloFcoJavier2122() {
         return precio_maxGutierrezMurilloFcoJavier2122;
     }
 
+    // Método setPrecio_maxGutierrezMurilloFcoJavier2122
     /**
-     * @param precio_maxGutierrezMurilloFcoJavier2122 the precio_maxGutierrezMurilloFcoJavier2122 to set
+     * Modifica el precio máximo del huevo sorpresa.
+     *
+     * @param precio_maxGutierrezMurilloFcoJavier2122 Precio máximo que puede
+     * tener un huevo sorpresa.
      */
-     // No se está usando un nombre convencional en el método.
+    // No se está usando un nombre convencional en el método.
     public void setPrecio_maxGutierrezMurilloFcoJavier2122(double precio_maxGutierrezMurilloFcoJavier2122) {
         this.precio_maxGutierrezMurilloFcoJavier2122 = precio_maxGutierrezMurilloFcoJavier2122;
     }
--- src/huevoSorpresa/Main.java
+++ src/huevoSorpresa/Main.java
@@ -5,50 +5,73 @@
 package huevoSorpresa;
 
 /**
+ * Clase main del programa, que define un objeto HuevoSorpresa con unos
+ * parámetros y realiza acciones de sacar huevos sorpresas y aumentar el precio.
  *
- * @author Profesor
+ * @author Francisco JAvier Gutiérrez Murillo
  */
 public class Main {
 
     /**
-     * @param args the command line arguments
+     * Método main. 
+     * Este método es el principal de ejecución del programa
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         HuevoSorpresa h_sorpresa;
-       
-        
-        h_sorpresa=new HuevoSorpresa(10,5,"peluche",10);
+
+        h_sorpresa = new HuevoSorpresa(10, 5, "peluche", 10);
+
         opera_PrecioGutierrezMurilloFcoJavier2122(h_sorpresa);
-        
+
         opera_UnidadesGutierrezMurilloFcoJavier2122(h_sorpresa);
-        
+
     }
 
+    /**
+     * Método opera_UnidadesGutierrezMurilloFcoJavier2122.
+     *
+     * En este método se va a intentar sacar 2 huevos sorpresa, como no hay
+     * dinero suficiente saltará una excepción y no se modificará el nº de
+     * unidades de la tienda.
+     *
+     * @param h_sorpresa objeto HuevoSorpresa.
+     *
+     * @throws Exception si la cantidad de dinero no es suficiente.
+     */
     private static void opera_UnidadesGutierrezMurilloFcoJavier2122(HuevoSorpresa h_sorpresa) {
         /*Vamos a intentar sacar 2 huevos sorpresa, como no hay dinero suficiente saltará
         una excepción y no se modificará el nº de unidades de la tienda*/
         try {
             System.out.println("Vamos a sacar huevos sorpresa");
             h_sorpresa.sacarHuevosSorpresas(2, 4, "llavero");
-            System.out.println("Operación realizada, ahora tiene "+h_sorpresa.obtenerUnidades()+" unidades en la tienda");
+            System.out.println("Operación realizada, ahora tiene " + h_sorpresa.obtenerUnidades() + " unidades en la tienda");
         } catch (Exception e) {
-            System.out.println("Error al sacar huevos sorpresa, el nº de unidades sigue siendo: "+h_sorpresa.obtenerUnidades());
-            
+            System.out.println("Error al sacar huevos sorpresa, el nº de unidades sigue siendo: " + h_sorpresa.obtenerUnidades());
+
         }
     }
 
+    /**
+     * Método opera_PrecioGutierrezMurilloFcoJavier2122.
+     *
+     * En este método se va a aumentar el precio 3€, como no supera el precio
+     * máximo establecido la operación tendrá éxito y el nuevo precio será de
+     * 8€.
+     *
+     * @param h_sorpresa objeto HuevoSorpresa.
+     *
+     * @throws Exception si la cantidad dinero a aumentar es errónea.
+     */
     private static void opera_PrecioGutierrezMurilloFcoJavier2122(HuevoSorpresa h_sorpresa) {
         /*Vamos a aumentar el precio 3€, como no supera el precio máximo establecido la operación
         tendrá éxito y el nuevo precio será de 8€*/
         try {
             System.out.println("Vamos a aumentar el precio de venta de un huevo sorpresa");
             h_sorpresa.aumentarPrecio(3);
-            System.out.println("El precio actual es "+h_sorpresa.obtenerPrecio()+" €");
+            System.out.println("El precio actual es " + h_sorpresa.obtenerPrecio() + " €");
         } catch (Exception e) {
             System.out.println("Error al aumentar el precio");
         }
     }
-    
-    
+
 }
